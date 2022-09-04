8896c54784704e8c103a2908ab3b43de8a0ac597
Murillo Mamud <murillomamud@gmail.com>
2022-09-04 19:46:57 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,66 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from test import calculate
+
+tests = ['AAAABAAAA','ABBA','BAAB','AABB','ABAB','AAAA','AAAAAAAAAAABBBBBBBBAAAAAAAAABABABABAAAAAAAAAAB']
+results = [6,1,1,2,0,3,34]
+count = 0
+if __name__ == '__main__': 
+    for letters in tests:     
+        print('----------------------------------------------------------')
+        print('Testing: ' + str(count))            
+        if calculate(letters) == results[count]:
+            print(letters + ' - ' + 'OK ' + str(calculate(letters)))
+        else:
+            print(letters + ' - ' + 'ERROR ' + str(calculate(letters)))
+        
+        count += 1
---
+++ readme.md
@@ -0,0 +1,7 @@
+https://www.hackerrank.com/challenges/alternating-characters/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
+
+to run it:
+
+'''
+python app.py
+'''
